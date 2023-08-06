1e5d23b3c32d7793527ab5a0d0ea1471999f2f0d
Shayan_Dx <shayanvafi0@gmail.com>
2023-08-06 14:07:51 +0330

    configured the admin pannel to show the users

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'is_active', 'is_staff')
