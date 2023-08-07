384a00c4109c1b3f1004566da2c264602798fa6d
Shayan_Dx <shayanvafi0@gmail.com>
2023-08-07 08:41:26 +0330

    (profiles_api) views has been modified to show staff users and non staff users

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+from rest_framework.views import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class AdminUsersView(APIView):
+    def get(self, request):
+        users = UserProfile.objects.filter(is_staff=True, is_active=True)
+        serializer = UserProfileSerializer(users, many=True, context={'request' : request})
+        return Response (serializer.data)
+    
+class NormalUsersView(APIView):
+    def get(self, request):
+        users = UserProfile.objects.filter(is_staff=False, is_active=True)
+        serializer = UserProfileSerializer(users, many=True, context={'request' : request})
+        return Response (serializer.data)
