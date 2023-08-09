ae106813f931f4dc48a4f270494ce56b155c8622
Shayan_Dx <shayanvafi0@gmail.com>
2023-08-09 11:38:06 +0330

    modified the serializers for upcoming features

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -6,7 +6,21 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ('id', 'first_name', 'last_name', 'email', 'is_active', 'is_staff')
+        extra_kwarge = {
+            'password' : {
+                'write_only' : True,
+                'style' : {'input_type' : 'password'}
+            }
+        }
     
+    def create(self, validated_data):
+        user = UserProfile.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            password=validated_data['password'],
+        )
+
+        return user
 
 class UserPostSerializer(serializers.Serializer):
     email = serializers.EmailField(max_length=255)
