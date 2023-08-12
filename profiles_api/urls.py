9f771049fd22236b8050c2ed1cec5744bd614e3e
Shayan_Dx <shayanvafi0@gmail.com>
2023-08-12 13:27:03 +0330

    added the login page in order to get use of token authentication

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UsersView, DetailView
+from .views import UsersView, DetailView, UserLoginAPIView
 
 
 urlpatterns = [
     path('', UsersView.as_view()),
     path('register/', UsersView.as_view()),
     path('update/<int:primary>/', DetailView.as_view(), name='user-update'),
+    path('login/', UserLoginAPIView.as_view()),
     path('<int:primary>/', DetailView.as_view()),
 ]
